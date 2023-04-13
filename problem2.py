7b6f04136e3c6f084e016bd16017300790ed50d7
CheremyP <96394051+CheremyP@users.noreply.github.com>
2023-04-13 18:37:55 +0200

    Problem 2
    Finished problem 2 and have the output file ass well

--- problem2.py
+++ problem2.py
@@ -10,24 +10,32 @@ DO NOT SHARE/DISTRIBUTE SOLUTIONS WITHOUT THE INSTRUCTOR'S PERMISSION
 
 import numpy as np
 from generate import GENERATE
-
+import re
 
 vocab = open("brown_vocab_100.txt")
 
 #load the indices dictionary
 word_index_dict = {}
-for i, line in enumerate(vocab):
-    #TODO: import part 1 code to build dictionary
+for index, line in enumerate(vocab):
+    # Import part 1 code to build dictionary
+    word = line.rstrip('\n').lower()
+    word_index_dict[word] = index
 
 f = open("brown_100.txt")
 
-counts = #TODO: initialize counts to a zero vector
+counts = np.zeros(813) # Initialize counts to a zero vector
 
-#TODO: iterate through file and update counts
+# Iterate through file and update counts
+with open("brown_100.txt", "r") as f:
+    for line in f:
+        words = line.split(" ") # Split line into words
+        for word in words:
+            word = word.lower()
+            if word in word_index_dict:
+                index = word_index_dict[word]
+                counts[index] += 1
 
-f.close()
-
-#TODO: normalize and writeout counts. 
-
-
-
+# Normalize and writeout counts. 
+probs = counts / np.sum(counts)
+np.savetxt("unigram_probs_100.txt", probs)
+loaded_probs = np.loadtxt("unigram_probs_100.txt")
