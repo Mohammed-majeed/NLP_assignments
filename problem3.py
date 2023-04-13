83449132f348850a78e541d31f0218f3591ba30f
mohammed-majeed <mohammed.y.majeed@gmail.com>
2023-04-13 20:24:51 +0200

    problem 3

--- problem3.py
+++ problem3.py
@@ -1,39 +1,55 @@
-#!/usr/bin/env python3
-
-"""
-NLP A2: N-Gram Language Models
-
-@author: Klinton Bicknell, Harry Eldridge, Nathan Schneider, Lucia Donatelli, Alexander Koller
-
-DO NOT SHARE/DISTRIBUTE SOLUTIONS WITHOUT THE INSTRUCTOR'S PERMISSION
-"""
-
 import numpy as np
 from sklearn.preprocessing import normalize
 from generate import GENERATE
-import random
 
 
-vocab = codecs.open("brown_vocab_100.txt")
+def get_ngrams(sentence, n):
+    """
+    Returns all n-grams in the given sentence.
+    """
+    words = sentence.split()
+    ngrams = []
+    for i in range(len(words) - n + 1):
+        ngrams.append(tuple(words[i:i+n]))
+    return ngrams
 
-#load the indices dictionary
+
+vocab = open("brown_vocab_100.txt")
+
+# Load the indices dictionary
 word_index_dict = {}
 for i, line in enumerate(vocab):
-    #TODO: import part 1 code to build dictionary
+    word_index_dict[line.strip()] = i
 
-f = codecs.open("brown_100.txt")
+f = open("brown_100.txt")
 
+n = 2
+counts = np.zeros((len(word_index_dict),) * n)
 
-counts = #TODO: initialize numpy 0s array
-
-
-#TODO: iterate through file and update counts
-
-#TODO: normalize counts
-
-
-#TODO: writeout bigram probabilities
-
-
+# Iterate through file and update counts
+for line in f:
+    lower_line = line.lower()
+    ngrams = get_ngrams(lower_line.strip(), n)
+    previous_word = "<s>"
+    for ngram in ngrams:
+        indices = [word_index_dict[w] for w in ngram]
+        counts[tuple(indices)] += 1
+        previous_word = ngram[0]
 
 f.close()
+
+# Normalize counts
+probs = normalize(counts, norm='l1', axis=1)
+
+np.savetxt("bigram_probs.txt", probs)
+
+
+# Print some bigram probabilities
+print("p(the | all) = ", probs[word_index_dict["all"], word_index_dict["the"]])
+print("p(jury | the) = ", probs[word_index_dict["the"], word_index_dict["jury"]])
+print("p(campaign | the) = ", probs[word_index_dict["the"], word_index_dict["campaign"]])
+print("p(calls | anonymous) = ", probs[word_index_dict["anonymous"], word_index_dict["calls"]])
+
+# use GENERATE
+gen = GENERATE(word_index_dict= word_index_dict, probs=probs, model_type='bigram',max_words=10,start_word='<s>')
+print(gen)
