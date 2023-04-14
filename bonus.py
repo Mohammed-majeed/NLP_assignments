6f02296be51e1c2a96ab9bf197854af9ede8ec12
mohammed-majeed <mohammed.y.majeed@gmail.com>
2023-04-14 19:31:14 +0200

    update all

--- bonus.py
+++ bonus.py
@@ -1,77 +1,113 @@
-import math
-import numpy as np
+# import math
+# import numpy as np
+# import nltk
+# from nltk.corpus import brown
+
+# def calculate_pmi(counts, word_index_dict, word_pair, total_words, min_count=10):
+#     """
+#     Calculates the Pointwise Mutual Information (PMI) for a given word pair.
+#     """
+#     word1, word2 = word_pair
+#     count_w1_w2 = counts[word_index_dict[word1], word_index_dict[word2]]
+#     count_w1 = counts[word_index_dict[word1]].sum()
+#     count_w2 = counts[:, word_index_dict[word2]].sum()
+    
+#     if count_w1_w2 == 0:
+#         return 0.0
+    
+#     p_w1_w2 = count_w1_w2 / total_words
+#     p_w1 = count_w1 / total_words
+#     p_w2 = count_w2 / total_words
+    
+#     if p_w1 == 0 or p_w2 == 0:
+#         return 0.0
+    
+#     pmi = math.log2(p_w1_w2 / (p_w1 * p_w2))
+    
+#     return pmi
+
+# # Load Brown corpus
+# corpus_words = brown.words()
+
+# # Count word frequencies
+# word_freqs = nltk.FreqDist(corpus_words)
+
+# # Filter words that occur less than 10 times
+# filtered_words = [word for word in word_freqs if word_freqs[word] >= 10]
+
+# # Create word index dictionary
+# word_index_dict = {word: i for i, word in enumerate(filtered_words)}
+
+# # Initialize counts matrix
+# counts = np.zeros((len(filtered_words), len(filtered_words)), dtype=np.uint32)
+
+# # Iterate through the corpus and count word pairs
+# for i in range(len(corpus_words)-1):
+#     word1 = corpus_words[i]
+#     word2 = corpus_words[i+1]
+#     if word1 in word_index_dict and word2 in word_index_dict:
+#         counts[word_index_dict[word1], word_index_dict[word2]] += 1
+
+# # Calculate PMI for all word pairs
+# total_words = counts.sum()
+# pmi_scores = []
+# for word_pair in [(word1, word2) for word1 in filtered_words for word2 in filtered_words]:
+#     pmi = calculate_pmi(counts, word_index_dict, word_pair, total_words)
+#     pmi_scores.append((word_pair, pmi))
+
+# # Sort word pairs based on PMI scores
+# pmi_scores.sort(key=lambda x: x[1], reverse=True)
+
+# # Get top 20 word pairs with highest PMI values
+# top_20 = pmi_scores[:20]
+
+# # Sort word pairs based on PMI scores in ascending order
+# pmi_scores.sort(key=lambda x: x[1])
+
+# # Get bottom 20 word pairs with lowest PMI values
+# bottom_20 = pmi_scores[:20]
+
+# # Print the top 20 and bottom 20 word pairs with PMI values
+# print("Top 20 word pairs with highest PMI values:")
+# for pair, pmi in top_20:
+#     print(pair[0], pair[1], pmi)
+
+# print("Bottom 20 word pairs with lowest PMI values:")
+# for pair, pmi in bottom_20:
+#     print(pair[0], pair[1], pmi)
+
+
+
 import nltk
 from nltk.corpus import brown
+from math import log2
 
-def calculate_pmi(counts, word_index_dict, word_pair, total_words, min_count=10):
-    """
-    Calculates the Pointwise Mutual Information (PMI) for a given word pair.
-    """
-    word1, word2 = word_pair
-    count_w1_w2 = counts[word_index_dict[word1], word_index_dict[word2]]
-    count_w1 = counts[word_index_dict[word1]].sum()
-    count_w2 = counts[:, word_index_dict[word2]].sum()
+# Get the counts of individual words and word pairs
+word_counts = brown.words()
+pair_counts = nltk.bigrams(word_counts)
+
+freq_words = nltk.FreqDist(word_counts)
+freq_pairs = nltk.FreqDist(pair_counts)
+
+# Calculate the PMI for each word pair
+pmi_values = {}
+N = freq_words.N()
+
+for pair in freq_pairs:
+    w1, w2 = pair
+    if freq_pairs[pair] < 10 or freq_words[w1] < 10 or freq_words[w2] < 10:
+        continue
     
-    if count_w1_w2 == 0:
-        return 0.0
-    
-    p_w1_w2 = count_w1_w2 / total_words
-    p_w1 = count_w1 / total_words
-    p_w2 = count_w2 / total_words
-    
-    if p_w1 == 0 or p_w2 == 0:
-        return 0.0
-    
-    pmi = math.log2(p_w1_w2 / (p_w1 * p_w2))
-    
-    return pmi
+    pmi = log2((freq_pairs[pair] / N) / (freq_words[w1] / N * freq_words[w2] / N))
+    pmi_values[pair] = pmi
 
-# Load Brown corpus
-corpus_words = brown.words()
-
-# Count word frequencies
-word_freqs = nltk.FreqDist(corpus_words)
-
-# Filter words that occur less than 10 times
-filtered_words = [word for word in word_freqs if word_freqs[word] >= 10]
-
-# Create word index dictionary
-word_index_dict = {word: i for i, word in enumerate(filtered_words)}
-
-# Initialize counts matrix
-counts = np.zeros((len(filtered_words), len(filtered_words)), dtype=np.uint32)
-
-# Iterate through the corpus and count word pairs
-for i in range(len(corpus_words)-1):
-    word1 = corpus_words[i]
-    word2 = corpus_words[i+1]
-    if word1 in word_index_dict and word2 in word_index_dict:
-        counts[word_index_dict[word1], word_index_dict[word2]] += 1
-
-# Calculate PMI for all word pairs
-total_words = counts.sum()
-pmi_scores = []
-for word_pair in [(word1, word2) for word1 in filtered_words for word2 in filtered_words]:
-    pmi = calculate_pmi(counts, word_index_dict, word_pair, total_words)
-    pmi_scores.append((word_pair, pmi))
-
-# Sort word pairs based on PMI scores
-pmi_scores.sort(key=lambda x: x[1], reverse=True)
-
-# Get top 20 word pairs with highest PMI values
-top_20 = pmi_scores[:20]
-
-# Sort word pairs based on PMI scores in ascending order
-pmi_scores.sort(key=lambda x: x[1])
-
-# Get bottom 20 word pairs with lowest PMI values
-bottom_20 = pmi_scores[:20]
-
-# Print the top 20 and bottom 20 word pairs with PMI values
+# Print the 20 word pairs with highest PMI values
+sorted_pmi = sorted(pmi_values.items(), key=lambda x: x[1], reverse=True)
 print("Top 20 word pairs with highest PMI values:")
-for pair, pmi in top_20:
+for pair, pmi in sorted_pmi[:20]:
     print(pair[0], pair[1], pmi)
 
-print("Bottom 20 word pairs with lowest PMI values:")
-for pair, pmi in bottom_20:
+# Print the 20 word pairs with lowest PMI values
+print("\nBottom 20 word pairs with lowest PMI values:")
+for pair, pmi in sorted_pmi[-20:]:
     print(pair[0], pair[1], pmi)
